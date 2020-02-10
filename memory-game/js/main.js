7357add6234c50ca4270c2716d423035a72cdf19
Sam Yi <samjyi95@gmail.com>
2020-02-10 04:54:03 -0800

    created game logic

--- memory-game/js/main.js
+++ memory-game/js/main.js
@@ -1,22 +1,43 @@
-function flipCard(cardId) {
-	console.log("User has flipped " + cards[cardId].rank);
-	//function above is accessing cards array with the key ref of "rank Id"
-	console.log("User has flipped " + cards[cardId].suit);
-	console.log("User has flipped " + cards[cardId].cardImage);
+function flipCard(card) {
+	// console.log("User has flipped " + cards[cardId].rank);
+	// //function above is accessing cards array with the key ref of "rank Id"
+	// console.log("User has flipped " + cards[cardId].suit);
+	// console.log("User has flipped " + cards[cardId].cardImage);
+	console.log('THIS', card);
+	let cardId = card.getAttribute('data-id')
 	cardsInPlay.push(cards[cardId].rank)
-	// step 3, not sure if I did that one right, we'll see.
-	checkForMatch();	
+	checkForMatch();
+	card.setAttribute('src', cards[cardId].cardImage)	
 }
 
 function checkForMatch() {
 	if (cardsInPlay.length >= 2 ) {
-		if (cardsInPlay[0] === cardsInPlay[1]) {
+		if (cardsInPlay[0].rank === cardsInPlay[1].rank) {
 			console.log("You found a match!");
 		} else {
 			console.log("Sorry, try again.");
 		}
 	}
 }
+
+function createBoard() {
+	for (let i = 0; i < cards.length; i++) {
+		board[i] = cards[i];
+	}
+}
+
+function createCards() {
+	for (let i = 0; i < cards.length; i++) {
+		let cardElement = document.createElement('img');
+		cardElement.setAttribute('src', 'images/back.png');
+		cardElement.setAttribute('data-id', i);
+		cardElement.addEventListener('click', function() {
+			flipCard(cardElement);
+		});
+		document.getElementById('game-board').appendChild(cardElement);
+	}
+}
+
 const cards = [
 {
 	rank: "queen",
@@ -39,6 +60,7 @@ const cards = [
 	cardImage: "images/king-of-diamonds.png",
 }
 ];
+var board = [];
 var cardsInPlay = [];
-flipCard(0);
-flipCard(2);
+createBoard();
+createCards();
