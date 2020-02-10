a0fc374e2533e793f12f5dd25c180ebf6a404cd4
Sam Yi <samjyi95@gmail.com>
2020-02-09 17:33:44 -0800

    created game logic

--- memory-game/js/main.js
+++ memory-game/js/main.js
@@ -1,6 +1,20 @@
-let cardOne = "Queen"
-let cardTwo = "King"
-let cardThree = "Queen"
-let cardFour = "King"
-console.log("User flipped" + cardOne);
-// Test
+var cards = ["queen", "queen", "king", "king"];
+
+var cardsInPlay = [];
+var cardOne = cards[0];
+var cardTwo = cards[1];
+var cardThree = cards[2];
+
+cardsInPlay.push(cardOne);
+cardsInPlay.push(cardTwo);
+cardsInPlay.push(cardThree);
+console.log("User flipped " + cardsInPlay[0]);
+console.log("User flipped " + cardsInPlay[2]);
+if (cardsInPlay.length >= 2 ) {
+	if (cardsInPlay[0] === cardsInPlay[1]) {
+		alert("You found a match!");
+	} else {
+		alert("Sorry, try again.");
+	}
+
+}
